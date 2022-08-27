51efc2a1cd3b0168dc1566035c7a62a3fdbe86f4
andressa-leite <dre_tl@hotmail.com>
2022-08-27 16:49:43 -0300

    div principal e logo adicionados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './Imagenes/logo.png';
 
 function App() {
   return (
-    <div className="App">
-      
-      
+    <div className='aplicacion-tareas'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={logo}
+          className= 'freecodecamp-logo'
+          alt= 'logo'
+        />
+    
+      </div>
+      <div
+        className='tareas-lista-principal'>
+          <h1>Mis Tareas</h1>
+
+      </div>
+        
+
     </div>
   );
 }
