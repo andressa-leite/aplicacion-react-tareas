03bc4d079e7c424881bb92f2473ec83c60d65029
andressa-leite <dre_tl@hotmail.com>
2022-08-28 21:40:20 -0300

    projeto finalizado quando clica sobre a tarefa ela fica riscada

--- src/componentes/TareaFormulario.js
+++ src/componentes/TareaFormulario.js
@@ -1,46 +1,43 @@
-import React, {useState } from "react";
+import React, { useState } from 'react';
 import '../hojas-de-estilo/TareaFormulario.css';
 import { v4 as uuidv4 } from 'uuid';
 
-function TareaFormulario(props){
+function TareaFormulario(props) {
 
-    const [input, setInput] = useState('');
+  const [input, setInput] = useState('');
 
-    const manejarCambio = e => {
-        setInput(e.target.value);
-        console.log("enviando");
+  const manejarCambio = e => {
+    setInput(e.target.value);
+  }
+
+  const manejarEnvio = e => {
+    e.preventDefault();
+    
+    const tareaNueva = {
+      id: uuidv4(),
+      texto: input,
+      completada: false
     }
 
-    const manejarEnvio = e => {
-        e.preventDefault();
-        
-       const tareaNueva = {
-            id: uuidv4(), //npm install uuid
-            texto: input,
-            completada: false
-        }
+    props.onSubmit(tareaNueva);
+  }
 
-        props.onSubmit(tareaNueva);
-    } 
-
-
-  
-
-    return(
-        <form className='tarea-formularo'
-              onSubmit={manejarEnvio}>
-            <input 
-                className='tarea-input'
-                type='text'
-                placeholder='Escribe una tarea'
-                name='texto'
-                onChange={manejarCambio}
-            />
-            <button className='tarea-boton'>
-                Agregar Tarea
-            </button>
-        </form>
-    )
+  return (
+    <form 
+      className='tarea-formulario'
+      onSubmit={manejarEnvio}>
+      <input 
+        className='tarea-input'
+        type='text'
+        placeholder='Escribe una Tarea'
+        name='texto'
+        onChange={manejarCambio}
+      />
+      <button className='tarea-boton'>
+        Agregar Tarea
+      </button>
+    </form>
+  );
 }
 
 export default TareaFormulario;
